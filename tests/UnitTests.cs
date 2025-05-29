5cf289e590452119d97a10dcddd3b9c488ba8265
Krzysztof Cwalina <kcwalina@microsoft.com>
2025-05-29 13:25:19 -0700

    cleaned up getarray

--- tests/UnitTests.cs
+++ tests/UnitTests.cs
@@ -34,12 +34,14 @@ public class Tests
         Assert.That(input.Names, Is.EqualTo(["my first building", "PI"]));
         Assert.That(input.Json.GetDouble("temperature"u8), Is.EqualTo(90d));    
         Assert.That(input.Json.GetString("category"u8), Is.EqualTo("facts"));
-        
-        // TODO: implement support for setting array elements
-        // TOOD: implement += <bytes> operator
-        // TODO: type hierarchy
-        // TOOD: what if there is JSON and CLR property returning complex object?
 
+        // Test GetArray<T> for numbers and names
+        double[] numbers = input.Json.GetArray<double>("numbers"u8);
+        Assert.That(numbers, Is.EqualTo(new double[] { 3.14, 7d }));
+        string[] names = input.Json.GetArray<string>("names"u8);
+        Assert.That(names, Is.EqualTo(new string[] { "my first building", "PI" }));
+
+        // serialize
         BinaryData json = ModelReaderWriter.Write(input);
 
         Assert.That(json.GetString("/category"u8), Is.EqualTo(input.Category));
@@ -49,11 +51,6 @@ public class Tests
         Assert.That(json.GetString("/names/1"u8), Is.EqualTo(input.Names[1]));
         Assert.That(json.GetDouble("/temperature"u8), Is.EqualTo(90d));
 
-        // Test GetArray<T> for numbers and names
-        var numbersArray = input.Json.GetArray<double>("numbers"u8);
-        Assert.That(numbersArray, Is.EqualTo(new double[] { 3.14, 7d }));
-        var namesArray = input.Json.GetArray<string>("names"u8);
-        Assert.That(namesArray, Is.EqualTo(new string[] { "my first building", "PI" }));
 
         OutputModel output = """
         {
