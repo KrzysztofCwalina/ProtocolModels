59b66b5ad7ca481d0160d69d2d15630accea493e
Krzysztof Cwalina <kcwalina@microsoft.com>
2025-07-09 09:37:09 -0700

    renames

---
+++ Profiling/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ClientModel.Primitives;
+
+ExtensionProperties _buffer = new();
+_buffer.Set("P1"u8, 5);
+
+Console.WriteLine("PRESS ENTER");
+Console.ReadLine();
+int found = 0;
+for (long i = 2; i <= 10000000000; i++)
+{
+    if (_buffer.Contains("P1"u8)) found++;
+}
+
